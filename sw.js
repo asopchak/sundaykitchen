c8087adff817f3a6bde726bf179fe0f83ef06b8f
Alex Sopchak <46893543+asopchak@users.noreply.github.com>
2026-07-03 17:12:31 -0400

    Deploy v14: sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Sunday Kitchen offline cache. Bump the version below whenever you upload a new index.html.
-const CACHE = 'sunday-kitchen-v13';
+const CACHE = 'sunday-kitchen-v14';
 self.addEventListener('install', function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(['./','./index.html']); }).then(function(){ return self.skipWaiting(); }));
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(['./','./index.html','./icon-warm.png','./icon-cool.png','./icon-dark.png']); }).then(function(){ return self.skipWaiting(); }));
 });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);})); }).then(function(){ return self.clients.claim(); }));
